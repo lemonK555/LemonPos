0b1f10b5f11d976a96bb42ac2bd8d272eac2f9e8
LemonK <932684335@qq.com>
2018-08-05 23:38:57 +0800

    增加了vuex页面的路由

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,19 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Pos from '@/components/page/Pos'
+import Count from '@/components/Count'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
-    	path: '/',
-      	name: 'Pos',
-      	component: Pos
+      path: '/',
+      name: 'Pos',
+      component: Pos
+    },{
+    	path:'/count',
+    	component:Count
     }
   ]
 })
